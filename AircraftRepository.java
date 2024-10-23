ef3f1be41531e6111b2d68d1dd78cf31215d05e9
kellystoyles68 <150394889+kellystoyles68@users.noreply.github.com>
2024-10-23 01:18:59 -0230

    add additional repositories

---
+++ AircraftRepository.java
@@ -0,0 +1,10 @@
+package com.keyin.domain;
+
+import com.keyin.domain.Aircraft;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AircraftRepository extends JpaRepository<Aircraft, Long> {
+    
+}
